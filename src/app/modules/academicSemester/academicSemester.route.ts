a9241ed5b9e184ff9ed50213af733054db3f2f3e
abir14016 <abir14016@gmail.com>
2023-11-22 15:47:21 +0600

    zod validation while creating an academic semester

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
 //route for creating an academic semester
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
 
 export const AcademicSemesterRoutes = router;
