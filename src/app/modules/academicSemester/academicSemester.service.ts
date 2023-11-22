5f61b5c058641ffe7ec412d8b61f73cc668da6ce
abir14016 <abir14016@gmail.com>
2023-11-22 15:55:48 +0600

    service for getting all academic semesters

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,4 +1,5 @@
 import { AcademicSemester, PrismaClient } from '@prisma/client';
+import { IGenericResponse } from '../../../interfaces/common';
 
 const prisma = new PrismaClient();
 
@@ -13,6 +14,24 @@ const insertIntoDB = async (
   return result;
 };
 
+//service for getting all academic semesters
+const getAllFromDB = async (): Promise<
+  IGenericResponse<AcademicSemester[]>
+> => {
+  const result = await prisma.academicSemester.findMany();
+  const total = await prisma.academicSemester.count();
+
+  return {
+    meta: {
+      total,
+      page: 1,
+      limit: 10,
+    },
+    data: result,
+  };
+};
+
 export const AcademicSemesterService = {
   insertIntoDB,
+  getAllFromDB,
 };
