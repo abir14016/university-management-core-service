b0f1986ae8048ca9b7eaaf5795e4e697a016bb37
abir14016 <abir14016@gmail.com>
2023-11-23 10:34:07 +0600

    controller for creating an academic faculty

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,21 @@
+import { AcademicFaculty } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterService } from '../academicSemester/academicSemester.service';
+
+//controller for creating an academic faculty
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicFaculty>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty created !!',
+    data: result,
+  });
+});
+
+export const AcademicFacultyController = {
+  insertIntoDB,
+};
