c1f327bde57f7163e3f2202377914612bcc4c005
abir14016 <abir14016@gmail.com>
2023-11-27 11:45:08 +0600

    controller for retriving all academic departments with searching, filtering, pagination and sorting

--- src/app/modules/academicDepartment/academicDepartment.controller.ts
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicDepartmentFilterableFields } from './academicDepartment.constants';
 import { AcademicDepartmentService } from './academicDepartment.service';
 
 //controller for creating an academic department
@@ -15,6 +17,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for retriving all academic departments with searching, filtering, pagination and sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicDepartmentFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await AcademicDepartmentService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'AcademicDepartments fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicDepartmentController = {
   insertIntoDB,
+  getAllFromDB,
 };
