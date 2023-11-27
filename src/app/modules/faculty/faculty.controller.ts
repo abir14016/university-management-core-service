cbe7c827ce3f892db3a1b7689772665dd7c07ccf
abir14016 <abir14016@gmail.com>
2023-11-27 13:19:53 +0600

    controller for getting all faculties

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { facultyFilterableFields } from './faculty.constants';
 import { FacultyService } from './faculty.service';
 
 //controller for inserting a faculty into DB
@@ -15,6 +17,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for getting all faculties with pagination, searching, filtering and sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, facultyFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await FacultyService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Faculties fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const FacultyController = {
   insertIntoDB,
+  getAllFromDB,
 };
