d04d83ba16df4a2e350fe29279bfdcdd6ea84455
abir14016 <abir14016@gmail.com>
2023-11-27 12:06:10 +0600

    controller for inserting a student inton DB

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { StudentService } from './student.service';
+
+// controller for inserting a student inton DB
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentService.insertIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student created successfully !',
+    data: result,
+  });
+});
+
+export const StudentController = {
+  insertIntoDB,
+};
