446a269737f522720981351d9ef13f4091e11981
abir14016 <abir14016@gmail.com>
2023-11-27 13:18:19 +0600

    service for getting all faculties

--- src/app/modules/faculty/faculty.service.ts
+++ src/app/modules/faculty/faculty.service.ts
@@ -1,5 +1,15 @@
-import { Faculty } from '@prisma/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Faculty, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import {
+  facultyRelationalFields,
+  facultyRelationalFieldsMapper,
+  facultySearchableFields,
+} from './faculty.constants';
+import { IFacultyFilterRequest } from './faculty.interface';
 
 //service for inserting a faculty into DB
 const insertIntoDB = async (data: Faculty): Promise<Faculty> => {
@@ -13,6 +23,80 @@ const insertIntoDB = async (data: Faculty): Promise<Faculty> => {
   return result;
 };
 
+//service for getting all faculties with pagination, searching, filtering and sorting
+const getAllFromDB = async (
+  filters: IFacultyFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Faculty[]>> => {
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: facultySearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (facultyRelationalFields.includes(key)) {
+          return {
+            [facultyRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.FacultyWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.faculty.findMany({
+    include: {
+      academicFaculty: true,
+      academicDepartment: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.faculty.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const FacultyService = {
   insertIntoDB,
+  getAllFromDB,
 };
