590674d73f2a62a51ec1ae28c98dcf2cf52002ad
abir14016 <abir14016@gmail.com>
2023-11-27 12:37:48 +0600

    controller for retriving all students with pagination, searching, filtering and sorting

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { studentFilterableFields } from './student.constants';
 import { StudentService } from './student.service';
 
 // controller for inserting a student inton DB
@@ -15,6 +17,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+// controller for retriving all students with pagination, searching, filtering and sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await StudentService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const StudentController = {
   insertIntoDB,
+  getAllFromDB,
 };
