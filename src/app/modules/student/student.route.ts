53e52ebcfd34f50ff7bfbf063b928cbb5f6cb033
abir14016 <abir14016@gmail.com>
2023-11-28 12:06:33 +0600

    student updated successfully

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -17,4 +17,11 @@ router.get('/', StudentController.getAllFromDB);
 // route for retriving single student from DB
 router.get('/:id', StudentController.getByIdFromDB);
 
+//route for updating student
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.update),
+  StudentController.updateIntoDB
+);
+
 export const StudentRoutes = router;
