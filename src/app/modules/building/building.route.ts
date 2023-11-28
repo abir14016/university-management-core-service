7de9626636c93f95510d96ce1acdd7aa41dad57d
abir14016 <abir14016@gmail.com>
2023-11-28 22:52:48 +0600

    all buildings retrived successfully with pagination, searching, filtering and sorting

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -6,4 +6,7 @@ const router = express.Router();
 //route for creating a building
 router.post('/', BuildingController.insertIntoDB);
 
+//route for getting all buildings with pagination, searching, filtering and sorting
+router.get('/', BuildingController.getAllFromDB);
+
 export const BuildingRoutes = router;
