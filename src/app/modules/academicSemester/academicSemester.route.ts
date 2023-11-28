70665294ad660ece3c2c21ed694c15a71badb514
abir14016 <abir14016@gmail.com>
2023-11-28 20:25:18 +0600

    assigning academic semester api endpoints permission by role

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
@@ -8,6 +10,7 @@ const router = express.Router();
 //route for creating an academic semester
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicSemesterValidation.create),
   AcademicSemesterController.insertIntoDB
 );
@@ -21,11 +24,16 @@ router.get('/:id', AcademicSemesterController.getDataById);
 //route for updating an academic semester by id
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicSemesterValidation.update),
   AcademicSemesterController.updateOneInDB
 );
 
 //route for deleting an academic semester by id
-router.delete('/:id', AcademicSemesterController.deleteByIdFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
+);
 
 export const AcademicSemesterRoutes = router;
