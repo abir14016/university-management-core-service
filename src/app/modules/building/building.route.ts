082dd7bb72a351e820ec192bd3956eb5b7dca7c6
abir14016 <abir14016@gmail.com>
2023-11-28 23:15:46 +0600

    assigning building api endpoints permission by role of user

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
 import { BuildingValidations } from './building.validation';
@@ -8,6 +10,7 @@ const router = express.Router();
 //route for creating a building
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(BuildingValidations.create),
   BuildingController.insertIntoDB
 );
@@ -21,11 +24,16 @@ router.get('/:id', BuildingController.getByIdFromDB);
 //route for updating a building
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(BuildingValidations.update),
   BuildingController.updateOneInDB
 );
 
 //route for deleting a building
-router.delete('/:id', BuildingController.deleteByIdFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  BuildingController.deleteByIdFromDB
+);
 
 export const BuildingRoutes = router;
