77ea50c581f71d7f6364d1714b67f470e82e1c13
abir14016 <abir14016@gmail.com>
2023-11-28 19:16:44 +0600

    assigning faculty api endpoints permission by role

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { FacultyController } from './faculty.controller';
 import { FacultyValidation } from './faculty.validation';
@@ -7,6 +9,7 @@ const router = express.Router();
 // route for inserting a faculty into DB
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(FacultyValidation.create),
   FacultyController.insertIntoDB
 );
@@ -20,11 +23,16 @@ router.get('/:id', FacultyController.getByIdFromDB);
 //route for updating a faculty
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(FacultyValidation.update),
   FacultyController.updateIntoDB
 );
 
 //route for deleting a faculty
-router.delete('/:id', FacultyController.deleteFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  FacultyController.deleteFromDB
+);
 
 export const facultyRoutes = router;
