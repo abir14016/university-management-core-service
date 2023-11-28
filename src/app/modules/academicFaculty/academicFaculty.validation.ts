1e3b64b63a8b11a682294fdc509758d2d4fd857b
abir14016 <abir14016@gmail.com>
2023-11-28 19:23:14 +0600

    zod validation for updating an academic faculty

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod';
 
+//zod validation for creating an academic faculty
 const create = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+  }),
+});
+
+//zod validation for updating an academic faculty
+const update = z.object({
   body: z.object({
     title: z.string({
       required_error: 'Title is required',
@@ -10,4 +20,5 @@ const create = z.object({
 
 export const AcademicFacultyValidation = {
   create,
+  update,
 };
