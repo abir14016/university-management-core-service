4c4ea12fabcbe1f0811bc5997caa3899edbcfa6f
abir14016 <abir14016@gmail.com>
2023-11-28 18:54:15 +0600

    controller for updating a faculty

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -43,8 +43,22 @@ const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for updating a faculty
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const payload = req.body;
+  const result = await FacultyService.updateIntoDB(id, payload);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Faculty updated successfully',
+    data: result,
+  });
+});
+
 export const FacultyController = {
   insertIntoDB,
   getAllFromDB,
   getByIdFromDB,
+  updateIntoDB,
 };
