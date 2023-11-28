eb5979d8830b9364f0877e702fd59fa762f3480d
abir14016 <abir14016@gmail.com>
2023-11-28 22:46:24 +0600

    service for getting all buildings with pagination, searching, filtering and sorting

--- src/app/modules/building/building.service.ts
+++ src/app/modules/building/building.service.ts
@@ -1,5 +1,10 @@
-import { Building } from '@prisma/client';
+import { Building, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import { buildingSearchableFields } from './building.constants';
+import { IBuildingFilterRequest } from './building.interface';
 
 //service for creating a building
 const insertIntoDB = async (data: Building): Promise<Building> => {
@@ -9,6 +14,58 @@ const insertIntoDB = async (data: Building): Promise<Building> => {
   return result;
 };
 
+//service for getting all buildings with pagination, searching, filtering and sorting
+const getAllFromDB = async (
+  filters: IBuildingFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Building[]>> => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm } = filters;
+
+  const andConditons = [];
+
+  if (searchTerm) {
+    andConditons.push({
+      OR: buildingSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  const whereConditons: Prisma.BuildingWhereInput =
+    andConditons.length > 0 ? { AND: andConditons } : {};
+
+  const result = await prisma.building.findMany({
+    skip,
+    take: limit,
+    where: whereConditons,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.building.count({
+    where: whereConditons,
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const BuildingService = {
   insertIntoDB,
+  getAllFromDB,
 };
