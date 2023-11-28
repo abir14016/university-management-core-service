5fd267aa2719493d1e423eb01aebf2712d9166be
abir14016 <abir14016@gmail.com>
2023-11-28 19:38:23 +0600

    assigning academic faculty api endpoints permssion by role

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyController } from './academicFaculty.controller';
 import { AcademicFacultyValidation } from './academicFaculty.validation';
@@ -8,6 +10,7 @@ const router = express.Router();
 //route for creating an academic faculty
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicFacultyValidation.create),
   AcademicFacultyController.insertIntoDB
 );
@@ -19,9 +22,18 @@ router.get('/', AcademicFacultyController.getAllFromDB);
 router.get('/:id', AcademicFacultyController.getDataById);
 
 //route for updating an academic faculty
-router.patch('/:id', AcademicFacultyController.updateOneInDB);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(AcademicFacultyValidation.update),
+  AcademicFacultyController.updateOneInDB
+);
 
 // route for deleting an academic faculty
-router.delete('/:id', AcademicFacultyController.deleteByIdFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicFacultyController.deleteByIdFromDB
+);
 
 export const AcademicFacultyRoutes = router;
