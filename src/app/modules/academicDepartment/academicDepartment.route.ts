4f5c603881fdd5fb02a942dbd0194c7096cae7bf
abir14016 <abir14016@gmail.com>
2023-11-28 20:03:48 +0600

    assigning academic department endpoints permission by role and delete academic department successfully

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicDepartmentController } from './academicDepartment.controller';
 import { AcademicDepartmentValidation } from './academicDepartment.validation';
@@ -7,6 +9,7 @@ const router = express.Router();
 //route for creating an academic department
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicDepartmentValidation.create),
   AcademicDepartmentController.insertIntoDB
 );
@@ -20,8 +23,16 @@ router.get('/:id', AcademicDepartmentController.getByIdFromDB);
 //route for updating an academic department
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicDepartmentValidation.update),
   AcademicDepartmentController.updateOneInDB
+);
+
+//route for deleting an academic department by id
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicDepartmentController.deleteByIdFromDB
 );
 
 export const AcademicDepartmentRoutes = router;
