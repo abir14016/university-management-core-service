c7addde73467631e15e135551da94a2d17c5cc33
abir14016 <abir14016@gmail.com>
2023-11-28 20:12:49 +0600

    zod validation for updating an academic semester by id

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 
+// zod validation for creating an academic semester
 const create = z.object({
   body: z.object({
     year: z.number({
@@ -20,6 +21,18 @@ const create = z.object({
   }),
 });
 
+// zod validation for updating an academic semester by id
+const update = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    code: z.string().optional(),
+    year: z.number().optional(),
+    startMonth: z.string().optional(),
+    endMonth: z.string().optional(),
+  }),
+});
+
 export const AcademicSemesterValidation = {
   create,
+  update,
 };
