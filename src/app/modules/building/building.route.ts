f0d3dd494f0133a5634750723f1cb5fd757383a6
abir14016 <abir14016@gmail.com>
2023-11-28 22:59:31 +0600

    single building fetched successfully by id

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidations } from './building.validation';
 
 const router = express.Router();
 
 //route for creating a building
-router.post('/', BuildingController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(BuildingValidations.create),
+  BuildingController.insertIntoDB
+);
 
 //route for getting all buildings with pagination, searching, filtering and sorting
 router.get('/', BuildingController.getAllFromDB);
+
+//route for getting single building by id
+router.get('/:id', BuildingController.getByIdFromDB);
 
 export const BuildingRoutes = router;
