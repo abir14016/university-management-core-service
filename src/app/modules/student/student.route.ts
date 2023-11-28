ed53b8300b0410ace2af34cbcebccad2b6137549
abir14016 <abir14016@gmail.com>
2023-11-28 15:12:08 +0600

    assigning student api endpoints permission by role

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { StudentController } from './student.controller';
 import { StudentValidation } from './student.validation';
@@ -7,6 +9,7 @@ const router = express.Router();
 //route for inserting a student into DB
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentValidation.create),
   StudentController.insertIntoDB
 );
@@ -20,11 +23,16 @@ router.get('/:id', StudentController.getByIdFromDB);
 //route for updating student
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentValidation.update),
   StudentController.updateIntoDB
 );
 
 // route for deleting a student from DB
-router.delete('/:id', StudentController.deleteFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  StudentController.deleteFromDB
+);
 
 export const StudentRoutes = router;
