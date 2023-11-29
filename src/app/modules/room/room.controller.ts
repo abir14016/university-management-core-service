cd710d8366a69aa98fde070139baf178c1b5717b
abir14016 <abir14016@gmail.com>
2023-11-29 14:46:05 +0600

    controller for getting all rooms with pagination, searching, filtering and sorting

--- src/app/modules/room/room.controller.ts
+++ src/app/modules/room/room.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { roomFilterableFields } from './room.constants';
 import { RoomService } from './room.service';
 
 //controller for creating a room
@@ -15,6 +17,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for getting all rooms with pagination, searching, filtering and sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, roomFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await RoomService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Rooms fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const RoomController = {
   insertIntoDB,
+  getAllFromDB,
 };
