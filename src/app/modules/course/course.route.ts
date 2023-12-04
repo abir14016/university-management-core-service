9290c498cc3b6101f98ace82f3fb29e8fc358b66
abir14016 <abir14016@gmail.com>
2023-12-04 06:46:25 +0600

    single course retrived successfully by id

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -9,4 +9,7 @@ router.post('/', CourseController.insertIntoDB);
 // route for retriving all courses with pagination, searching, filtering and sorting
 router.get('/', CourseController.getAllFromDB);
 
+//route for getting single course by id
+router.get('/:id', CourseController.getByIdFromDB);
+
 export const CourseRoutes = router;
