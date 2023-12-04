ef80a7d773b93bec33182b939bdcf83edea5c22c
abir14016 <abir14016@gmail.com>
2023-12-04 06:27:53 +0600

    validation added for course module

---
+++ src/app/modules/course/course.validation.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod';
+
+//zod validation for creating a course
+const create = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+    code: z.string({
+      required_error: 'Code is required',
+    }),
+    credits: z.number({
+      required_error: 'Credits is required',
+    }),
+    preRequisiteCourses: z
+      .array(
+        z.object({
+          courseId: z.string({}),
+        })
+      )
+      .optional(),
+  }),
+});
+
+//zod validation for updating a course
+const update = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    code: z.string().optional(),
+    credits: z.number().optional(),
+    preRequisiteCourses: z
+      .array(
+        z.object({
+          courseId: z.string({}),
+          isDeleted: z.boolean({}).optional(),
+        })
+      )
+      .optional(),
+  }),
+});
+
+export const CourseValidation = {
+  create,
+  update,
+};
