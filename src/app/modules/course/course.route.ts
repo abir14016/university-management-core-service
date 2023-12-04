243c07cfb0492fcdbf16d782e3cd5c0dbe7a2cd6
abir14016 <abir14016@gmail.com>
2023-12-04 06:39:18 +0600

    all courses retrived successfully !

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -6,4 +6,7 @@ const router = express.Router();
 // route for inserting a course into DB
 router.post('/', CourseController.insertIntoDB);
 
+// route for retriving all courses with pagination, searching, filtering and sorting
+router.get('/', CourseController.getAllFromDB);
+
 export const CourseRoutes = router;
