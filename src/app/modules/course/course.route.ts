38aea0b21ce3f890d6d568e961653f83a2197b9c
abir14016 <abir14016@gmail.com>
2023-12-28 05:36:19 +0600

    role based permission and validation added for a course to assign/remove faculty/faculties

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -37,8 +37,18 @@ router.delete(
 );
 
 //route for assigning faculties into a course
-router.post('/:id/assign-faculties', CourseController.assignFaculties);
+router.post(
+  '/:id/assign-faculties',
+  validateRequest(CourseValidation.assignOrRemoveFaculties),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  CourseController.assignFaculties
+);
 
 //route for removing faculty/faculties from a course
-router.delete('/:id/remove-faculties', CourseController.removeFaculties);
+router.delete(
+  '/:id/remove-faculties',
+  validateRequest(CourseValidation.assignOrRemoveFaculties),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  CourseController.removeFaculties
+);
 export const CourseRoutes = router;
