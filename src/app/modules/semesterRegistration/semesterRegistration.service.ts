16cd7a0ae92813082b5ef8cca16aa005a004e799
abir14016 <abir14016@gmail.com>
2023-12-28 06:37:42 +0600

    service for retriving all semester registration with pagination, searching, filtering & sorting

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
+  Prisma,
   SemesterRegistration,
   SemesterRegistrationStatus,
 } from '@prisma/client';
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import {
+  semesterRegistrationRelationalFields,
+  semesterRegistrationRelationalFieldsMapper,
+  semesterRegistrationSearchableFields,
+} from './semesterRegistration.constants';
+import { ISemesterRegistrationFilterRequest } from './semesterRegistration.interface';
 
 //service for creating semester registration
 const insertIntoDB = async (
@@ -37,6 +48,79 @@ const insertIntoDB = async (
   return result;
 };
 
+//service for retriving all semester registration with pagination, searching, filtering & sorting
+const getAllFromDB = async (
+  filters: ISemesterRegistrationFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<SemesterRegistration[]>> => {
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: semesterRegistrationSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (semesterRegistrationRelationalFields.includes(key)) {
+          return {
+            [semesterRegistrationRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.SemesterRegistrationWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.semesterRegistration.findMany({
+    include: {
+      academicSemester: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.semesterRegistration.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const SemesterRegistrationService = {
   insertIntoDB,
+  getAllFromDB,
 };
