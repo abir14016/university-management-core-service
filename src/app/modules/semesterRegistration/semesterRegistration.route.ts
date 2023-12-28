e45882479f3b064aad999ec5957d5771a0437515
abir14016 <abir14016@gmail.com>
2023-12-28 06:42:10 +0600

    all semester registration fetched successfully git add .

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -6,4 +6,7 @@ const router = express.Router();
 //route for creating semester registration
 router.post('/', SemesterRegistrationController.insertIntoDB);
 
+//route for retriving all semester registration with pagination, searching, filtering & sorting
+router.get('/', SemesterRegistrationController.getAllFromDB);
+
 export const SemesterRegistrationRoutes = router;
