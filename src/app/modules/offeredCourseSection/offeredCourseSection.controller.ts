e1641bc13d9aa1374b2ff633f431a0bf4ba12ff6
abir14016 <abir14016@gmail.com>
2023-12-30 02:35:50 +0600

    controller for creating offeredCourseSection

---
+++ src/app/modules/offeredCourseSection/offeredCourseSection.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { OfferedCourseSectionService } from './offeredCourseSection.service';
+
+//controller for creating offeredCourseSection
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await OfferedCourseSectionService.insertIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Offered Course Section created',
+    data: result,
+  });
+});
+
+export const OfferedCourseSectionController = {
+  insertIntoDB,
+};
