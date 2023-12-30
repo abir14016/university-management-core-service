81ea2903688fae077d58022f1dbda09403f2362c
abir14016 <abir14016@gmail.com>
2023-12-30 17:00:17 +0600

    controller for getting all offeredCourseSection with pagination, searching, filtering & sorting

--- src/app/modules/offeredCourseSection/offeredCourseSection.controller.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { offeredCourseSectionFilterableFields } from './offeredCourseSection.constants';
 import { OfferedCourseSectionService } from './offeredCourseSection.service';
 
 //controller for creating offeredCourseSection
@@ -15,6 +17,24 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for getting all offeredCourseSection with pagination, searching, filtering & sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, offeredCourseSectionFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await OfferedCourseSectionService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourseSections fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const OfferedCourseSectionController = {
   insertIntoDB,
+  getAllFromDB,
 };
