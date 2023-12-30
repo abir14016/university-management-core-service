b277e86fc73041be41ca79aab302c19ecb5c4547
abir14016 <abir14016@gmail.com>
2023-12-30 16:59:21 +0600

    service for getting all offeredCourseSection with pagination, searching, filtering & sorting

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -1,8 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { OfferedCourseSection } from '@prisma/client';
+import { OfferedCourseSection, Prisma } from '@prisma/client';
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import {
+  offeredCourseSectionRelationalFields,
+  offeredCourseSectionRelationalFieldsMapper,
+  offeredCourseSectionSearchableFields,
+} from './offeredCourseSection.constants';
+import { IOfferedCourseSectionFilterRequest } from './offeredCourseSection.interface';
 
 //service for creating offeredCourseSection
 const insertIntoDB = async (data: any): Promise<OfferedCourseSection> => {
@@ -28,6 +37,83 @@ const insertIntoDB = async (data: any): Promise<OfferedCourseSection> => {
   return result;
 };
 
+//service for getting all offeredCourseSection with pagination, searching, filtering & sorting
+const getAllFromDB = async (
+  filters: IOfferedCourseSectionFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<OfferedCourseSection[]>> => {
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: offeredCourseSectionSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (offeredCourseSectionRelationalFields.includes(key)) {
+          return {
+            [offeredCourseSectionRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.OfferedCourseSectionWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.offeredCourseSection.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+    include: {
+      offeredCourse: {
+        include: {
+          course: true,
+        },
+      },
+    },
+  });
+  const total = await prisma.offeredCourseSection.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const OfferedCourseSectionService = {
   insertIntoDB,
+  getAllFromDB,
 };
