9a02174e853ee9416684411edc801871b8eb79ec
abir14016 <abir14016@gmail.com>
2023-12-31 02:19:05 +0600

    all offeredCourseClassSchedule retrived successfully

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
@@ -6,4 +6,7 @@ const router = express.Router();
 //route for creating OfferedCourseClassSchedule
 router.post('/', OfferedCourseClassScheduleController.insertIntoDB);
 
+//route for getting all offeredCourseClassSchedule with pagination, searching, filtering & sorting
+router.get('/', OfferedCourseClassScheduleController.getAllFromDB);
+
 export const OfferedCourseClassScheduleRoutes = router;
