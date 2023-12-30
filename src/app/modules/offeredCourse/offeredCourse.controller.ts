8b3fb09af2c9748e9ad79105199af0472686e1c0
abir14016 <abir14016@gmail.com>
2023-12-30 16:25:35 +0600

    controller for getting all offred course with pagination, searching, filtering & sorting

--- src/app/modules/offeredCourse/offeredCourse.controller.ts
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { offeredCourseFilterableFields } from './offeredCourse.constant';
 import { offeredCourseService } from './offeredCourse.service';
 
 //controller for creating offred course
@@ -15,6 +17,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for getting all offred course with pagination, searching, filtering & sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, offeredCourseFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await offeredCourseService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourses fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const OfferedCourseController = {
   insertIntoDB,
+  getAllFromDB,
 };
