f38562f84314813224b8d20bdb7ccbe0a80f6aa0
abir14016 <abir14016@gmail.com>
2024-01-06 18:34:48 +0600

    controller for studentEnrolledCourse

---
+++ src/app/modules/studentEnrolledCourse/studentEnrolledCourse.controller.ts
@@ -0,0 +1,79 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { studentEnrolledCourseFilterableFields } from './studentEnrolledCourse.constants';
+import { StudentEnrolledCourseService } from './studentEnrolledCourse.service';
+
+//controller for creating studentEnrolledCourse
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentEnrolledCourseService.insertIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'StudentEnrolledCourse created successfully',
+    data: result,
+  });
+});
+
+//controller for getting all studentEnrolledCourses with pagination, searching, filtering & sorting
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentEnrolledCourseFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await StudentEnrolledCourseService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'StudentEnrolledCourses fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+//controller for getting single studentEnrolledCourse
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await StudentEnrolledCourseService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'StudentEnrolledCourse fetched successfully',
+    data: result,
+  });
+});
+
+//controller for updating studentEnrolledCourse
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await StudentEnrolledCourseService.updateOneInDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'StudentEnrolledCourse updated successfully',
+    data: result,
+  });
+});
+
+//controller for deleting studentEnrolledCourse
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await StudentEnrolledCourseService.deleteByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'StudentEnrolledCourse deleted successfully',
+    data: result,
+  });
+});
+
+export const StudentEnrolledCourseController = {
+  insertIntoDB,
+  getAllFromDB,
+  getByIdFromDB,
+  updateOneInDB,
+  deleteByIdFromDB,
+};
