bd05b34db8983fbf60d31d8d447163c6651b4814
abir14016 <abir14016@gmail.com>
2024-01-10 02:53:14 +0600

    controller for retrieve list of students enrolled in your course for a specific academic semester and course section [for faculty]

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -106,6 +106,29 @@ const myCourses = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+//controller for retrieve list of students enrolled in your course for a specific academic semester and course section [for faculty]
+const getMyCourseStudents = catchAsync(async (req: Request, res: Response) => {
+  const user = (req as any).user;
+  const filters = pick(req.query, [
+    'academicSemesterId',
+    'courseId',
+    'offeredCourseSectionId',
+  ]);
+  const options = pick(req.query, ['limit', 'page']);
+  const result = await FacultyService.getMyCourseStudents(
+    filters,
+    options,
+    user
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Faculty course students fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const FacultyController = {
   insertIntoDB,
   getAllFromDB,
@@ -115,4 +138,5 @@ export const FacultyController = {
   assignCourses,
   removeCourses,
   myCourses,
+  getMyCourseStudents,
 };
